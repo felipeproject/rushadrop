3ef123fb5aa3b3982b9da39ca63b0f794a870a18
felipeproject <andrefelipenascimento96@gmail.com>
2025-08-03 15:07:11 -0300

    Auto commit em 2025-08-03 15:07

--- js/configGlobal.js
+++ js/configGlobal.js
@@ -3,9 +3,9 @@
 const DIAS = ['DAY1', 'DAY2', 'DAY3', 'DAY4'];
 const MAPAS_POR_DIA = [
   ['Erangel', 'Taego', 'Miramar'],
-  ['Vikendi', 'Erangel', 'Deston'],
-  ['Sanhok', 'Deston', 'Erangel'],
-  ['Erangel', 'Vikendi', 'Miramar']
+  ['Deston', 'Vikendi', 'Erangel'],
+  ['Sanhok', 'Rondo', 'Taego'],
+  ['Deston', 'Erangel', 'Miramar']
 ];
 
 export const rodadas = Object.fromEntries(
@@ -23,3 +23,6 @@ export const PONTOS_POR_COLOCACAO = {
   1: 15, 2: 12, 3: 10, 4: 8,
   5: 6, 6: 4, 7: 2
 };
+
+// Se quiser usar DIAS e MAPAS_POR_DIA diretamente:
+export { DIAS, MAPAS_POR_DIA };
